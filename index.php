c90f5110fde7d6eeee1503664d3b752c769ae9bf
Marcus Vinícius de Ávila Couto <marcusvnac@gmail.com>
2016-06-11 21:49:06 -0300

    Initial commit (#1)

---
+++ index.php
@@ -0,0 +1,275 @@
+<!DOCTYPE html>
+<html lang="pt-BR" class=" js flexbox canvas canvastext webgl no-touch geolocation postmessage websqldatabase indexeddb hashchange history draganddrop websockets rgba hsla multiplebgs backgroundsize borderimage borderradius boxshadow textshadow opacity cssanimations csscolumns cssgradients cssreflections csstransforms csstransforms3d csstransitions fontface generatedcontent video audio localstorage sessionstorage webworkers applicationcache svg inlinesvg smil svgclippaths">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta charset="UTF-8">
+	<title>Estrelarte</title>
+	<meta name="robots" content="index,follow">
+	<meta name="description" content="Arte em feltros. Confecção de lembranças, enfeites e presentes personalizados em feltro, especialmente para você.">
+	<meta name="keywords" content="arte,feltro,costura,festas,batizado,aniversários,presentes,decoração">
+	
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">	
+	<link rel="stylesheet" id="onetone-owl-carousel-css" href="./css/owl.carousel.css" type="text/css" media="">
+	<link rel="stylesheet" id="onetone-owl-theme-css" href="./css/owl.theme.css" type="text/css" media="">
+	<link rel="stylesheet" id="onetone-main-css" href="./css/style.css" type="text/css" media="all">
+	<link rel="stylesheet" id="font-adjust-css" href="./css/font-adjust.css" type="text/css" media="">
+	<!-- slick jQuery Carrousel -->
+	<link rel="stylesheet" type="text/css" href="http://cdn.jsdelivr.net/jquery.slick/1.6.0/slick.css"/>
+	<link rel="stylesheet" type="text/css" href="http://cdn.jsdelivr.net/jquery.slick/1.6.0/slick-theme.css"/>
+	
+	<style id="onetone-main-inline-css" type="text/css">
+		.site{no-repeat top left fixed;}a,.site-logo a:hover,.site-navigation a:hover,.widget a:hover,.entry-title a:hover,.entry-meta a:hover,.loop-pagination a:hover,.page_navi a:hover,.site-footer a:hover,.home-navigation > ul > li.current > a > span,.home-navigation li a:hover,.home-navigation li.current a,.home-footer a:hover,#back-to-top,#back-to-top span{color:#666;}#back-to-top {border:1px solid #666;}mark,ins,.widget #wp-calendar #today{background:#666; }::selection{background:#666 !important;}::-moz-selection{background:#666 !important;}body{margin:0px;}
+
+		.section-banner {
+		}
+
+		@media screen and (max-width:919px) {
+		.section-banner {
+		}
+		}
+	</style>	
+	
+	<style type="text/css" id="custom-background-css">body.custom-background { background-color: #ffffff; }</style>
+</head>
+
+<body class="blog custom-background">
+	<div class="home-site">
+		<header class="home-header sticky-header" style="position: static;">
+			<div class="home-logo onetone-logo ">
+				<a href="#">
+					<img src="./images/logo.png" alt="OneTone">
+				</a>
+			</div>
+			
+			<a class="home-navbar navbar" href="javascript:;"></a>
+			<nav class="home-navigation top-nav">
+				<ul id="menu-main" class="main-nav">
+					<li class="onetone-menuitem current active"><a id="onetone-home" href="#home" style="top: 0px;"><span>Início</span></a></li>
+					<li class="onetone-menuitem"><a id="onetone-services" href="#services" style="top: 0px;"><span>Nossa Arte</span></a></li>
+					<li class="onetone-menuitem"><a id="onetone-about" href="#about" style="top: 0px;"><span>Sobre</span></a></li>
+					<li class="onetone-menuitem"><a id="onetone-gallery" href="#gallery" style="top: 0px;"><span>Galeria</span></a></li>
+					<li class="onetone-menuitem"><a id="onetone-contact" href="#contact" style="top: 0px;"><span>Contato</span></a></li>
+					<li class="onetone-menuitem"><a id="onetone-custom-section" href="#custom-section" style="top: 0px;"><span>Orçamento</span></a></li>										
+				</ul>        
+			</nav>
+			<div class="clear"></div>
+		</header>    
+		
+		<div class="container home-wrapper">
+			<section id="home" class="section section-banner onetone-home" style="position: relative; z-index: 99; height: 700px;">
+				<div class="home-container page_container">
+					<div class="banner-box"><br>
+						<h1>Estrelarte</h1>
+						<span>A arte em suas mãos</span>
+						<p style="text-align:center;color:#008CCF">Confecção de lembranças, enfeites e presentes personalizados em feltro, especialmente para você.</p>						
+						<ul class="banner-sns">
+							<li><img src="./images/estrela.png"</li>
+						</ul>
+					</div>            
+				</div>
+				<div class="clear"></div>  
+			</section>
+			<section id="services" class="section  onetone-services " style=" background:url(./images/home-bg02.jpg)  repeat top left scroll;">
+				<div class="home-container page_container">
+					<h1>Nossa Arte</h1>
+						<div class="one_third"><!-- Font Awesome Icon-->
+							<i class="fa fa-3 fa-child"></i>
+							<h3>Crianças</h3>
+							Lembracinhas de nascimento do bebê e batizado.
+						</div>
+						<div class="one_third"><!-- Font Awesome Icon-->
+							<i class="fa fa-3 fa-gift"></i>
+							<h3>Presentes</h3>
+							Muitos mimos para você presentear seus familiares, amigos e a você. Almofadas, máscara de dormir, chaveiros e muito mais!!!
+						</div>
+						<div class="one_third last"><!-- Font Awesome Icon-->
+							<i class="fa fa-3 fa-calendar-o"></i>
+							<h3>Datas Comemorativas</h3>
+							Do Réveillon ao Natal você encontra a lembrança certa.
+						</div>            
+				</div>
+				<div class="clear"></div>
+			</section>
+			<section id="about" class="section section-about onetone-parallax onetone-about " style="background:url(./images/map.jpg) repeat top left scroll;background-attachment:fixed;background-position:50% 0;background-repeat:no-repeat;">
+				<div class="home-container page_container">
+					<h1>Sobre</h1>
+					<div class="two_third">
+						<h3>Quem sou</h3>
+						<p style="text-align:left">Olá! Me chamo Priscila e sou apaixonada pelo artesanato desde criança. Influenciada pela minha mãe,
+							comecei a fazer bordados em ponto cruz, depois passei para o bordado em fitas e almofadas estilo 'casinha de abelha'. 
+						<p style="text-align:left">Apaixonada pelo artesanato, me aventurei no feltro, vendendo meus produtos para a família e amigos, e agora apresento minha arte ao mundo!<p>
+						<p style="text-align:left">Seja bem vindo ao céu encantado da Estrelarte!</p>
+					</div>
+					<div class="one_third last">
+						<h3>Informações</h3>
+						<ul>
+							<li class="info-phone">+55 (31) 98692-6165</li>
+							<li class="info-address">Belo Horizonte, MG, Brasil</li>
+							<li class="info-email"><a target="_blank" href="mailto:contato@estrelarte.com.br?subject=Contato pelo Site">contato@estrelarte.com.br</a></li>
+							<li class="info-website"><a href="#">estrelarte.com.br</a></li>
+							<li class="info-facebook"><a target="_blank" href="https://www.facebook.com/EstrelarteFeltro">fbl.me/estrelarte</a></li>
+						</ul>
+					</div>            
+				</div>
+				<div class="clear"></div>
+			</section>
+			<section id="gallery" class="section  onetone-gallery " style=" background:url(./images/home-bg02.jpg)  repeat top left scroll;">
+				<div class="home-container page_container">
+					<h1>Galeria</h1>
+					
+					<div class="portfolio-list" id="gallery">
+						<div><img class="port-img" src="./images/gallery/g1.jpg" alt="" data-zoom-image="./images/gallery/g1.jpg"></div>
+						<div><img class="port-img" src="./images/gallery/g2.jpg" alt=""></div>
+						<div><img class="port-img" src="./images/gallery/g3.jpg" alt=""></div>
+						<div><img class="port-img" src="./images/gallery/g4.jpg" alt=""></div>
+						<div><img class="port-img" src="./images/gallery/g5.jpg" alt=""></div>
+						<div><img class="port-img" src="./images/gallery/g6.jpg" alt=""></div>
+						<div><img class="port-img" src="./images/gallery/g7.jpg" alt=""></div>
+						<div><img class="port-img" src="./images/gallery/g8.jpg" alt=""></div>
+						<div><img class="port-img" src="./images/gallery/g9.jpg" alt=""></div>
+						<div><img class="port-img" src="./images/gallery/g10.jpg" alt=""></div>
+						<div><img class="port-img" src="./images/gallery/g11.jpg" alt=""></div>
+						<div><img class="port-img" src="./images/gallery/g12.jpg" alt=""></div>
+						<div><img class="port-img" src="./images/gallery/g13.jpg" alt=""></div>
+						<div><img class="port-img" src="./images/gallery/g14.jpg" alt=""></div>
+						<div><img class="port-img" src="./images/gallery/g15.jpg" alt=""></div>
+						<div><img class="port-img" src="./images/gallery/g16.jpg" alt=""></div>
+						<div><img class="port-img" src="./images/gallery/g17.jpg" alt=""></div>
+						<div><img class="port-img" src="./images/gallery/g18.jpg" alt=""></div>
+						<div><img class="port-img" src="./images/gallery/g19.jpg" alt=""></div>
+						<div><img class="port-img" src="./images/gallery/g20.jpg" alt=""></div>
+					</div>					
+				</div>
+				<div class="clear"></div>
+			</section>
+			<section id="contact" class="section  onetone-contact " style=" background:url(./images/home-bg03.jpg)  repeat top left scroll;">
+				<div class="home-container page_container">
+					<h1>Contato</h1>
+					<p class="contact-text">Entre em contato deixando aqui sua mensagem de sugestão ou crítica.<br>Responderei o mais rápido possível. Obrigada.<br><i class="fa fa-1 fa-envelope"></i></p>					
+
+					<div class="contact-area">
+						<?php
+							if($_REQUEST['formmail_submit']){
+								$to = "contato@estrelarte.com.br";
+								$subject = "Contato pelo Form do Site";
+								$name = preg_replace("([\r\n])", "", $_POST['name']);
+								$email = preg_replace("([\r\n])", "", $_POST['email']);
+								$message = preg_replace("([\r\n])", "", $_POST['message']);
+
+								$match = "/(bcc:|cc:|content\-type:)/i";
+								if (preg_match($match, $name) ||
+								 preg_match($match, $email) ||
+								 preg_match($match, $message)) {
+									die("Header injection detected.");
+								}
+
+								$error = $name == '' ? '<div class = "error">Por favor, informe o seu nome.</div>' : '';
+								$error = $email == '' ? '<div class = "error">Por favor, informe o seu e-mail.</div>' : '';
+								$error = $message == '' ? '<div class = "error">Por favor, informe o motivo do seu contato.</div>' : '';
+
+								if(!$error){
+									$strMessage .= "Nome: " . $_POST[name] . "\n";
+									$strMessage .= "E-mail: " . $_POST[email] . "\n";
+									$strMessage .= "Motivo do contato: " . $_POST[message] . "\n";
+
+									$headers = "From: " . $subject . " <". $to . ">\n";
+									$headers .= "Mime-Version: 1.0\n";
+									$headers .= "Content-Type: text/plain; charset=ISO-8859-1\n";
+									$headers .= "Content-Transfer-Encoding: 8bit\n";
+									$headers .= "Return-Path: <" . $to . ">\n";
+
+									$isHideForm = @mail($to, htmlentities($subject), $strMessage, $headers);
+								}
+							}
+								if( $isHideForm )
+									echo "Sua mensagem foi enviada.";
+								else{
+
+							?>
+
+							<div>
+							<?php echo($error);?>
+								<script type = 'text/javascript'>
+									function checkForm(){
+										var message = '';
+										if(document.getElementById('name').value == '')
+											message += '\t - Informe o seu nome\n';
+
+										if(document.getElementById('email').value == '')
+											message += '\t - Informe o seu e-mail\n';
+
+										if(document.getElementById('message').value == '')
+											message += '\t - Informe o motivo do seu contato.\n';
+
+										if(message != ''){
+											alert('Por favor, corriga os seguintes problemas para enviar sua mensagem:\n\n' + message);
+											return false;
+										}
+										return true;
+									}	
+								</script>
+								
+							<form class="contact-form" method ="post" action ="<?php echo($_SERVER['PHP_SELF']);?>" onsubmit="return checkForm()">
+								<input id="name" tabindex="1" size="22" type="text" value="" name="name" placeholder="Informe seu nome">
+								<input id="email" tabindex="2" size="22" type="text" value="" name="email" placeholder="Informe seu e-mail">
+								<textarea id="message"  tabindex="3" cols="39" name="message" rows="5"></textarea>
+								<input type="submit" value="Enviar">
+								<input type = 'hidden' name = 'formmail_submit' value = 'submit' />
+							</form>
+							</div>
+
+						<?php } ?>
+
+					</div>            
+				</div>
+				<div class="clear"></div>
+			</section>
+			<section id="custom-section" class="section  onetone-custom-section " style=" background:url(./images/home-bg02.jpg)  repeat top left scroll;">
+				<div class="home-container page_container">
+					<h1>Orçamento</h1>
+					Gostou da minha arte? Então faça logo seu orçamento aqui. Terei o maior prazer de atender você!
+					<br>					
+					<a style="text-decoration:underline; !important" target="_blank" href="https://docs.google.com/forms/d/1aFn-SdljOurC8rbfQt01-PROTolaxD7lpljvPo-4sl8/viewform">Clique aqui</a>  para enviar uma solicitação de orçamento.					
+					<br><br>
+				</div>
+				<div class="clear"></div>
+			</section>
+			<div class="clear"></div>  
+		</div>
+		
+		<footer class="home-footer">
+			<div class="home-site-info">
+				Estrelarte - A arte em suas mãos
+			</div>
+		</footer>
+	</div>
+	
+	<a href="javascript:;">
+		<div class="top" id="back-to-top">
+			<span class="fa fa-arrow-up"></span>
+			<span>Sobe</span>
+		</div>
+    </a>
+	
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+	<script type="text/javascript" src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+	<script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/jquery-scrollTo/1.4.14/jquery.scrollTo.min.js"></script>
+	<script type="text/javascript" src="http://cdn.jsdelivr.net/jquery.slick/1.6.0/slick.min.js"></script>
+	
+	<script type="text/javascript" src="./scripts/modernizr.custom.js"></script>	
+	<script type="text/javascript" src="./scripts/jquery.parallax-1.1.3.js"></script>	
+	<script type="text/javascript" src="./scripts/owl.carousel.js"></script>
+	<script type="text/javascript" src="./scripts/onetone.js"></script>	
+	<script type="text/javascript" src="./scripts/slick.carousel.js"></script>
+	
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-59489328-1', 'auto');
+	  ga('send', 'pageview');
+	</script>
+</html>
